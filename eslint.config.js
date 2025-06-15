6d4b7484be3cafaea0af2fd94c4f426d004e4b8a
mulekick <moolekick@gmail.com>
2025-06-15 04:34:48 +0200

    fix typedoc options

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
 import muleslint from "@mulekick/eslint-config-muleslint";
-// eslint-disable-next-line node/no-missing-import
+// eslint-disable-next-line import/no-unresolved, n/no-extraneous-import
 import typescript from "typescript-eslint";
 
+// .eslintignore doesn't work with flat configs
 export default typescript.config(...muleslint, {
     ignores: [ `**/node_modules/**`, `**/dist/**`, `**/build/**` ]
 });
